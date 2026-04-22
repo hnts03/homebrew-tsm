841a372ed474fe09e805dfc7c94f26e6964be836
hnts03 <chlrjsdn0320@gmail.com>
2026-04-22 10:34:41 +0900

    chore: bump tsm to v0.1.1

--- Formula/tsm.rb
+++ Formula/tsm.rb
@@ -1,19 +1,14 @@
 # Homebrew formula for tsm
-# To use before official tap:
-#   brew install --formula ./Formula/tsm.rb
-#
-# Once a tap is published:
-#   brew tap hnts03/tmux-session-manager
-#   brew install tsm
+# brew tap hnts03/tsm
+# brew install tsm
 
 class Tsm < Formula
   desc "Lightweight fzf-based tmux session manager"
   homepage "https://github.com/hnts03/tmux-session-manager"
-  version "0.1.0"
+  version "0.1.1"
 
-  # Update url and sha256 after first GitHub release
   url "https://github.com/hnts03/tmux-session-manager/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "459b7ffbe3bfbc7c8be51e3c19ad032dac13522f3d5e89ff2bda8a2a96d8f753"
+  sha256 "96f29464ba18c2dfed431f285ff327ff4ae145d66ca44468d5fae3e99ee807f8"
 
   license "MIT"
 
